a7490fcd1805db0aa9576a8f00d4877dd45c8e9b
Jaya <akulajayateja4444@gmail.com>
2023-05-10 17:47:34 +0530

    changed the style of calendar button

--- components/calendar2.tsx
+++ components/calendar2.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Button} from 'react-native';
+import {Text, TouchableOpacity, StyleSheet} from 'react-native';
 import DatePicker from 'react-native-date-picker';
 
 const Calendar2 = () => {
@@ -8,7 +8,11 @@ const Calendar2 = () => {
 
   return (
     <>
-      <Button title="Open" onPress={() => setOpen(true)} />
+      <TouchableOpacity
+        style={styles.buttonStyle}
+        onPress={() => setOpen(true)}>
+        <Text style={styles.buttonTextStyle}>Open calendar</Text>
+      </TouchableOpacity>
       <DatePicker
         modal
         mode="date"
@@ -25,5 +29,28 @@ const Calendar2 = () => {
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  buttonStyle: {
+    width: '100%',
+    height: 50,
+    elevation: 5,
+    borderRadius: 10,
+    borderWidth: 1,
+    backgroundColor: 'white',
+    alignSelf: 'center',
+    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingLeft: 15,
+    paddingRight: 15,
+  },
+  buttonTextStyle: {
+    fontWeight: '600',
+    color: 'black',
+    justifyContent: 'center',
+  },
+});
 
 export default Calendar2;
